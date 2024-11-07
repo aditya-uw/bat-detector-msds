315d6398b5695e16e3efee2d6b1bb2931d0396bb
Aditya Krishna <adkris1002@gmail.com>
2024-11-07 04:31:54 +0000

    added large instance notebook

--- daily_notebooks/20241106__perform_parallel_parameter_sweep.py
+++ daily_notebooks/20241106__perform_parallel_parameter_sweep.py
@@ -211,7 +211,7 @@ if __name__ == '__main__':
         cfg['tmp_dir'].mkdir(parents=True, exist_ok=True)
 
     print(f'generating segments in {cfg["tmp_dir"]}')
-    segmented_file_paths = generate_segmented_paths(selected_wav_paths[:2], cfg)
+    segmented_file_paths = generate_segmented_paths(selected_wav_paths[:1], cfg)
     file_path_mappings = batdetect2_pipeline.initialize_mappings(segmented_file_paths, cfg)
     input = file_path_mappings[:]
     num_rows = multiprocessing.cpu_count()
@@ -219,8 +219,7 @@ if __name__ == '__main__':
     time_taken_test3 = np.zeros((num_rows, num_cols))
     for i in range(num_rows):
         num_processes = int(i+1)
-        if num_processes!=1:
-            torch.set_num_threads(1)
+        torch.set_num_threads(min(2, np.floor(multiprocessing.cpu_count()/num_processes).astype(int)))
         for j in range(num_cols):
             pool = multiprocessing.Pool(processes=num_processes)
             chunksize_custom = int(j+1)
@@ -233,16 +232,16 @@ if __name__ == '__main__':
             end = time.time()
             time_taken_test3[i,j] = end-start
 
-            np.save(f'{Path(__file__).parent}/20241105__2xl_instance_single_file_{len(input)}_{num_cols}x{num_rows}_computation.npy')
+            np.save(f'{Path(__file__).parent}/20241106__large_instance_{len(input)}chunks_{num_cols}x{num_rows}_computation.npy', time_taken_test3)
 
             plt.figure(figsize=(8,6))
             plt.title(f'{len(input)} segments fixed; num_threads=1')
-            plt.imshow(time_taken_test3)
+            plt.imshow(time_taken_test3/60)
             plt.ylabel('Num_processes (processors assigned)')
             plt.xlabel('Chunksize (chunks per processor)')
             plt.yticks(np.arange(num_rows)-0.5, np.arange(num_rows)+1)
             plt.xticks(np.arange(num_cols)-0.5, np.arange(num_cols)+1)
             plt.grid(which='both')
-            plt.colorbar(label='Time taken (s)')
-            plt.savefig(f'{Path(__file__).parent}/20241105__large_instance_single_file_{len(input)}_{num_cols}x{num_rows}_computation.png', bbox_inches='tight')
+            plt.colorbar(label='Time taken (min)')
+            plt.savefig(f'{Path(__file__).parent}/20241106__large_instance_{len(input)}chunks_{num_cols}x{num_rows}_computation.png', bbox_inches='tight')
             plt.close()
