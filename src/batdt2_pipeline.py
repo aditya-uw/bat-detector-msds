830a23d8224a8f9e9dd1b873b72c002fbcd27634
Aidan Lee <aidan.lee@outlook.com>
2025-03-19 14:58:58 -0700

    Added kmeans training set into repository

--- src/batdt2_pipeline.py
+++ src/batdt2_pipeline.py
@@ -353,7 +353,7 @@ def gather_features_of_interest(dets, kmean_welch, audio_file):
 
 def open_and_get_call_info(audio_file, dets):
     welch_key = 'all_locations'
-    output_dir = Path(f'{Path(__file__).parent}/../../duty-cycle-investigation/data/generated_welch/{welch_key}')
+    output_dir = Path(f'{Path(__file__).parent}/../kmeans_training_set')
     output_file_type = 'top1_inbouts_welch_signals'
     welch_data = pd.read_csv(output_dir / f'2022_{welch_key}_{output_file_type}.csv', index_col=0, low_memory=False)
     k = 2
